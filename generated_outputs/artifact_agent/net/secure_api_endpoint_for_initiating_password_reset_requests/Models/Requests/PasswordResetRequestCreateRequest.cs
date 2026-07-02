3f3ae976c8e9c0032c2bce3a961f1a99dcb39748
itsshubhpandey <apple@192.168.1.143>
2026-07-02 17:24:31 +0530

    aaded techstack

--- generated_outputs/artifact_agent/net/secure_api_endpoint_for_initiating_password_reset_requests/Models/Requests/PasswordResetRequestCreateRequest.cs
+++
@@ -1,7 +0,0 @@
-namespace SecureApiEndpoint.Models.Requests;
-
-public class PasswordResetRequestCreateRequest
-{
-    public string Email { get; set; } = string.Empty;
-    public string? ResetToken { get; set; }
-}
--- generated_outputs/artifact_agent/net/secure_api_endpoint_for_initiating_password_reset_requests/Services/Interfaces/IPasswordResetRequestService.cs
+++
@@ -1,9 +0,0 @@
-using SecureApiEndpoint.Models.Requests;
-using SecureApiEndpoint.Models.Responses;
-
-namespace SecureApiEndpoint.Services.Interfaces;
-
-public interface IPasswordResetRequestService
-{
-    Task<PasswordResetRequestResponse> CreatePasswordResetRequestAsync(PasswordResetRequestCreateRequest request);
-}
